a19c775b541e2f84a1185e9e589ae3b78c15838d
Pedro Henrique <pedro_ph_x@hotmail.com>
2022-09-07 13:58:19 -0300

    New files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code>.
         </p>
         <a
           className="App-link"
---
+++ src/components/Banner/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./index.css";
+
+export default function Banner() {
+  return (
+    <header className="banner">
+      <img alt="banner" src="/images/banner.png" />
+    </header>
+  );
+}
